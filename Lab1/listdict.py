7feb958cfc02ec3dae226ada7b03fa75e85698a0
A_Nama <145112688+A-Nama@users.noreply.github.com>
2025-03-05 15:07:45 +0530

    Add files via upload

---
+++ Lab1/listdict.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5]
+print("Original list: ", my_list)
+my_list.append(6)
+my_list.remove(1)
+
+print("Updated list: ", my_list)
+
+my_dict = {"Alice" : 1, "Melody" : 2, "Trina" : 3}
+print("Original dictionary: ", my_dict)
+my_dict["Victoria"] = 4
+
+del my_dict["Trina"]
+print("Updated dictionary: ", my_dict)
---
+++ Lab1/subtractmatrices.py
@@ -0,0 +1,17 @@
+def subtract_matrices(mat1, mat2):
+  rows, cols = len(mat1), len(mat1[0])
+  result = [[mat1[i][j] - mat2[i][j] for j in range(cols)] for i in range(rows)]
+  return result
+  
+  
+rows, cols = map(int, input("Enter the number of rows and columns: ").split())
+print("Enter the elements of the first matrix row-wise: ")
+matrix1 = [list(map(int, input().split())) for _ in range(rows)]
+print("Enter the elements of the second matrix row-wise: ")
+matrix2 = [list(map(int, input().split())) for _ in range(rows)]
+
+result = subtract_matrices(matrix1, matrix2)
+print("Resultant matrix: ")
+for row in result: 
+  print(row)
+  
